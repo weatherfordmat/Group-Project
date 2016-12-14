bf31916269a046ceca0bdfab0ee2c9a65a6c1276
Joel Walker <Joel Walker>
2016-12-14 10:24:29 -0600

    added a css file for forms

--- assets/javascript/forms.js
+++ assets/javascript/forms.js
@@ -4,5 +4,24 @@ var config = {
     databaseURL: "https://ontheway-9edc3.firebaseio.com",
     storageBucket: "ontheway-9edc3.appspot.com",
     messagingSenderId: "422948488078"
-  };
-  firebase.initializeApp(config);
+ };
+ firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+ $(document).ready(function(e) {
+     $('.submit').click(function() {
+         var name = $('.custName').val().trim();
+         var contact = $('.contact').val().trim();
+         var contactCell = $('.contactCell').val().trim();
+         var address= $('.custAddress').val().trim();
+           database.ref().push({
+         	name_db: name,
+            contact_db: contact,
+            contactCell_db: contactCell,
+         	address_db: address,
+         	});
+         return false;
+  	});
+ });

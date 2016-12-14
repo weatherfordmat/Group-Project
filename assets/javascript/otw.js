f6b077e1265f3f744ce292abe6329d7d32c9d5ee
Matthew Weatherford <weatherfordmat@gmail.com>
2016-12-14 00:58:57 -0600

    added a function for sending texts

--- assets/javascript/otw.js
+++ assets/javascript/otw.js
@@ -4,5 +4,25 @@ var config = {
     databaseURL: "https://ontheway-9edc3.firebaseio.com",
     storageBucket: "ontheway-9edc3.appspot.com",
     messagingSenderId: "422948488078"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+$(document).ready(function() {
+	
+});
+
+
+
+
+
+
+/* Warning, each text is 0.0065 cents. Use with caution.
+to use => sendText('12017016880', '12817430153', 'It works!');
+*/
+function sendText(from, to, text) {
+    $.post({
+        url: "https://rest.nexmo.com/sms/json?api_key=09348072&api_secret=c6e4241595ec75fe&from=" + from + "&to=" + to + "&text=" + text,
+    }).done(function(response) {
+        console.log(response);
+    });
+};

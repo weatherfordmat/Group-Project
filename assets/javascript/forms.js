7063b6a585b65e05f6f1488f53c72f807bc40d70
Joel Walker <Joel Walker>
2016-12-13 20:51:48 -0600

    Joel first commit

---
+++ assets/javascript/forms.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBRDe64D5qCPMtJpLSHCL8G8_VvdpvMQgw",
+    authDomain: "ontheway-9edc3.firebaseapp.com",
+    databaseURL: "https://ontheway-9edc3.firebaseio.com",
+    storageBucket: "ontheway-9edc3.appspot.com",
+    messagingSenderId: "422948488078"
+  };
+  firebase.initializeApp(config);

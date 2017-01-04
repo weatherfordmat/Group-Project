bf515d6e77fd880822667dbeeb6738f80d098795
Matthew Weatherford <weatherfordmat@gmail.com>
2017-01-04 17:39:03 +0100

    added mult map view

--- assets/javascript/otw.js
+++ assets/javascript/otw.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
                 } else {
                     var result = (response.history.rows[0].elements[0].duration.text).replace(/\D/g, ' ');
                 }
-                $('tbody').append("<tr class='eachRow'><td><span style='color: gold' class='glyphicon glyphicon-star-empty' aria-hidden='true'></td></span><td class='companyName'>" + childSnapshot.val().name_db + "</td><td>" + address + "</td><td>" + cell + "</td><td><button data-lat=" + lat + " data-lng=" + lng + " class='btn btn-primary dur'>" + result + "</button></td><td style='color: green'; class='status'>open</td><td data-notes='" +childSnapshot.val().notes_db +"'>" + notes + "</td></tr>")
+                $('tbody').append("<tr class='eachRow'><td><span style='color: gold' class='glyphicon glyphicon-star-empty' aria-hidden='true'></td></span><td class='companyName'>" + childSnapshot.val().name_db + "</td><td>" + address + "</td><td>" + cell + "</td><td><button data-lat=" + lat + " data-lng=" + lng + " class='btn btn-primary dur'>" + result + "</button></td><td style='color: green'; class='status'>open</td><td data-notes='" + childSnapshot.val().notes_db + "'>" + notes + "</td></tr>")
                     //show lowest value time;
                 if ($('.table tr').length === numberRecords) {
                     readtable();
@@ -67,51 +67,46 @@ $(document).ready(function() {
                 [4, "desc"]
             ]
         });
-
         //show the entire note if its larger than 10 characters;
         //Should there be a way to reverse it?
-        $('.table').on('click','.more', function() {
+        $('.table').on('click', '.more', function() {
             $(this).parent().html($(this).parent().attr('data-notes'))
         });
-
         //click on a specific company to route to. . .
         var clickedYet = false;
         getModal('.modal2', '.dur', '.close2');
         $('.dur').append(' mins');
         $('.dur').on('click', function() {
-                
-                var that = this;
-                $('.table').on('click', '.eachRow', function() {
-                    $('#companyName').html($(this).find('td:eq(1)').text())
-                    $('#addressPopup').html($(this).find('td:eq(2)').text());
-                    $('#numPopup').html($(this).find('td:eq(3)').text());
-                    $('#timeToArrival').html($(this).find('td:eq(4)').text() + " away");
-                    $('#notesPopup').text($(this).find('td:eq(6)').text());
-                });
-                window.initMap = function initMap() {
-                  
-                    var routeTo = {
-                        lat: Number($(that).attr('data-lat')),
-                        lng: Number($(that).attr('data-lng'))
-                    };
-                    var map = new google.maps.Map(document.getElementById('map'), {
-                        zoom: 15,
-                        center: routeTo
-                    });
-                    var market = new google.maps.Marker({
-                        position: routeTo,
-                        map: map,
-                    });
-                }
-                if (!clickedYet) {
-                    clickedYet = true;
-                    $('.script2').html('<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBytQb1gFbAwEOvaDH-PJmh2lds7q2a8CM&callback=initMap">')
-                }
-                else {
-                    initMap();
-                }          
+            var that = this;
+            $('.table').on('click', '.eachRow', function() {
+                $('#companyName').html($(this).find('td:eq(1)').text())
+                $('#addressPopup').html($(this).find('td:eq(2)').text());
+                $('#numPopup').html($(this).find('td:eq(3)').text());
+                $('#timeToArrival').html($(this).find('td:eq(4)').text() + " away");
+                $('#notesPopup').text($(this).find('td:eq(6)').text());
             });
-            //change the star to be filled or not
+            window.initMap = function initMap() {
+                var routeTo = {
+                    lat: Number($(that).attr('data-lat')),
+                    lng: Number($(that).attr('data-lng'))
+                };
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 15,
+                    center: routeTo
+                });
+                var market = new google.maps.Marker({
+                    position: routeTo,
+                    map: map,
+                });
+            }
+            if (!clickedYet) {
+                clickedYet = true;
+                $('.script2').html('<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBytQb1gFbAwEOvaDH-PJmh2lds7q2a8CM&callback=initMap">')
+            } else {
+                initMap();
+            }
+        });
+        //change the star to be filled or not
     }
     $('.table').on('click', '.glyphicon', function() {
             if ($(this).hasClass('glyphicon-star-empty')) {
@@ -132,19 +127,26 @@ $(document).ready(function() {
     $('.subButton').click(function() {
             login();
         })
-    //show map view & hide list view;
-    $('#toggleMap').on('click', function() {
-        $('.panel').animate({
+        //show map view & hide list view;
+    $('#toggleList').on('click', function() {
+        $('.panel-body').show();
+        
+        $('#map_wrapper').animate({
             left: "+=100",
             height: "toggle"
         }, 500);
     });
     //show list view and hide map view;
-    $('#toggleList').on('click', function() {
-        $('.panel').animate({
+    $('#toggleMap').on('click', function() {
+        $('.panel-body').hide();
+        $('#map_wrapper').animate({
             left: "+=100",
             height: "toggle"
         }, 500);
+        $('.panel').append('<div id="map_wrapper"><div id="map_canvas" class="mapping"></div></div>')
+        var script = document.createElement('script');
+        script.src = "https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+        document.body.appendChild(script);
     });
     getModal('#myModal', '.addCustomer', '.close');
 });
@@ -225,5 +227,65 @@ function getModal(modal, buttonOpen, buttonClose) {
     });
     $(buttonClose).click(function() {
         modal.css('display', 'none');
+    });
+}
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+    // Multiple Markers
+    rootRef.on("value", function(childSnapshot) {
+        var name = childSnapshot.val().name_db;
+        var address = childSnapshot.val().address_db;
+        var cell = childSnapshot.val().contactCell_db;
+        var dest = address;
+        var lat = childSnapshot.val().lat;
+        var lng = childSnapshot.val().lng;
+        total = [];
+        childSnapshot.forEach(function(childSnapshot) {
+            total = [
+                [childSnapshot.val().name_db, childSnapshot.val().lat, childSnapshot.val().lng]
+            ];
+            descript = [
+                ['<div class="info_content"><h2><strong>' +childSnapshot.val().name_db + '</strong></h2><ul><em><li>Address: ' +childSnapshot.val().address_db +'</li></em><li>Notes: ' +childSnapshot.val().notes_db +'</li></ul></div>']
+            ];
+            
+            var markers = total;
+            // Info Window Content
+            var infoWindowContent = descript;
+            // Display multiple markers on a map
+            var infoWindow = new google.maps.InfoWindow(),
+                marker, i;
+            // Loop through our array of markers & place each one on the map  
+            for (i = 0; i < markers.length; i++) {
+                var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+                bounds.extend(position);
+                marker = new google.maps.Marker({
+                    position: position,
+                    map: map,
+                    title: markers[i][0]
+                });
+                // Allow each marker to have an info window    
+                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                    return function() {
+                        infoWindow.setContent(infoWindowContent[i][0]);
+                        infoWindow.open(map, marker);
+                    }
+                })(marker, i));
+                // Automatically center the map fitting all markers on the screen
+                map.fitBounds(bounds);
+            }
+            // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+            var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+                this.setZoom(12);
+                google.maps.event.removeListener(boundsListener);
+            });
+        });
     });
 }
